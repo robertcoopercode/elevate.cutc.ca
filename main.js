f9c4d49dd44797bb1d23cde935732b17a433c06d
TranBrian10 <tranbrian10@gmail.com>
2017-02-23 19:50:42 -0500

    Fix merge

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
     countdown();
 
     //Disabled ticket button click scrolls to email block
-    $("#ticket_button.button_disabled").on("click", function(e) {
-        e.preventDefault();
+    // $("#ticket_button.button_disabled").on("click", function(e) {
+    //     e.preventDefault();
 
-        $("html, body").animate({
-            scrollTop: $("#email_block").offset().top
-        }, 1000);
-    });
+    //     $("html, body").animate({
+    //         scrollTop: $("#email_block").offset().top
+    //     }, 1000);
+    // });
 
     //Updates all current years
     $(".current_year").text(new Date().getFullYear());
