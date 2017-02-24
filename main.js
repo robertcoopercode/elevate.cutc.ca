32d4eb71d146349d5e69c02eafadb9e8729f4121
TranBrian10 <tranbrian10@gmail.com>
2017-02-24 18:22:02 -0500

    Added BMO to silver sponsors

--- main.js
+++ main.js
@@ -12,15 +12,6 @@ $(document).ready(function() {
     timer_seconds = $("#timer_seconds");
     countdown();
 
-    //Disabled ticket button click scrolls to email block
-    // $("#ticket_button.button_disabled").on("click", function(e) {
-    //     e.preventDefault();
-
-    //     $("html, body").animate({
-    //         scrollTop: $("#email_block").offset().top
-    //     }, 1000);
-    // });
-
     //Updates all current years
     $(".current_year").text(new Date().getFullYear());
 
